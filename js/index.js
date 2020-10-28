6e21fd4c954e2b573b75dcf3d21349f840636e4c
Fidan Novruzova <yf@Fidans-MacBook-Pro.local>
2020-10-27 20:03:15 -0700

    still working

--- js/index.js
+++ js/index.js
@@ -1,29 +1,35 @@
 // Your code goes here
+//load 1
 window.addEventListener('load', (event) => {
     alert('Welcome');
   });
+  //mouseover 2
 let heading = document.querySelector(".main-navigation");
 let imgs = document.querySelectorAll('img');
 heading.addEventListener("mouseover",(event) => {
 heading.style.backgroundColor = "Beige";
 heading.style.transition = ".3s"
 })
+//mouseout 3
 heading.addEventListener("mouseout",(event)=>{
     heading.style.backgroundColor = "initial";
 })
-
+//mouseenter 4
 Array.from(document.links).forEach(link => {
-    link.addEventListener("mouseover",()=>{
+    link.addEventListener("mouseenter",(event)=>{
         link.style.color = "grey";
-        link.style.transform = "scale(1.2)"
+        link.style.transform = "scale(1.2)";
+        event.preventDefault();
     })
-    link.addEventListener("mouseout",()=>{
+    //mouseleave 5
+    link.addEventListener("mouseleave",(event)=>{
         link.style = "initial";
-        
+        event.preventDefault();
     })
-})
-//2
 
+})
+
+//click 6
 imgs.forEach( img1 => {
     img1.addEventListener("click",() => {
     img1.style.borderRadius = "100%"
@@ -32,19 +38,34 @@ img1.addEventListener("mouseout",() => {
     img1.style = "initial";
 })
 })
+//keydown 7
+let h = document.querySelectorAll("h2");
+h.forEach(hs => {
+    document.addEventListener("keydown",() => {
+hs.style.color = "red";
+    })
+    //keyup 8
+   document.addEventListener("keyup",() => {
+        hs.style.color = "initial";
+            })
+})
 
-let p = document.querySelectorAll('p');
+let butt = document.querySelectorAll(".btn");
 
-// p.forEach(word => {
-//     word.addEventListener("select",(event) => {
-//         const selected = event.target.innerText.substring(event.target.selectionStart,event.target.selectionEnd);
-//         alert(`You selected: ${selected}`);
-//         })
-// })
-function logSelection(event) {
+let but2 = document.querySelector(".destination:nth-of-type(2) .btn");
+console.log(but2);
+butt.forEach(btn => {
+    btn.addEventListener("mouseover", (event) => {
+        event.target.textContent = "Click me";
+        
+}),btn.addEventListener("mouseleave",(event)=>{
+    event.target.textContent = 'Sign me up!';
     
-    const selection = event.target.innerText.substring(event.target.selectionStart, event.target.selectionEnd);
-    alert( `You selected: ${selection}`);
-  }
+});
+});
 
-  const log = document.getElementsByClassName('text-content');
+but2.addEventListener("mouseover", (event) => {
+    
+    event.target.style.backgroundColor = "orange";
+    event.stopPropagation();
+})
